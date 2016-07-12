4c971f6eff514a28ee550464f471cef3e4f5e0aa
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2016-07-11 22:22:34 -0500

    Added mongoDB integration. Added datetimepicker to submit form. Added trainer level persistence. Added trainer_level and datetime to submit params.

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var dataBase = {}; // Reference
 
-// app.use(bodyParser());
+var mongoURI = process.env.MONGODB_URI; 
+// Connect to the db
+MongoClient.connect(mongoURI, function(error, db) {
+  if(!error) {
+    console.log("We are connected");
+    dataBase = db;
 
-// app.use(bodyParser.json())
-// var urlencodedParser = bodyParser.urlencoded({ extended: false })
+    db.createCollection('test', function(error, collection) {
+        if (!error) {
+            console.log("Collection retrieved: ", collection);
+        } else {
+            console.error("Error creating collection...", error);
+        }
+    });
 
-var entry = {
-        // {
-        //     pokemon: "001",
-        //     location: {
-        //         latitude: "",
-        //         longitude: ""
-        //     },
-        //     submitter: {
-        //         user: {
-        //             id: "98765",
-        //             username: "ABBA"
-        //         } 
-        //     }
-        // }
-}
+    db.createCollection('encounters', function(error, collection) {
+        if (!error) {
+            console.log("Collection retrieved: ", collection);
+        } else {
+            console.error("Error creating collection...", error);
+        }
+    });
+    // db.createCollection('test', {strict:true}, function(err, collection) {});
+
+  } else {
+    console.error("Error connecting to DB", error)
+  }
+});
 
 app.set('port', (process.env.PORT || 5000));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+// Shared lib scripts
+app.use('/libs', express.static(__dirname + '/libs'));
 
 app.get('/', function(request, response) {
   response.render('pages/game');
@@ -37,16 +50,49 @@ app.get('/submit', function(request, response) {
     response.render('pages/submit_finding');
 });
 
+function encounter (pokemon_id, pokemon_name, latitude, longitude, datetime, trainer_level) {
+    this.pokemon_id = pokemon_id;
+    this.pokemon_name = pokemon_name; 
+    this.latitude = latitude; 
+    this.longitude = longitude; 
+    this.datetime = datetime; 
+    this.trainer_level = trainer_level;
+}
+
 app.post('/submit', function(request, response) {
     if (request.query.entry) {
         var id = Math.floor(Math.random() * 10000);
-        var newEntry = JSON.parse(request.query.entry);
-        entry [id] = newEntry;
+        var entry = JSON.parse(request.query.entry);
+        console.log("entry: ",entry);
+        newEntries = [];
+        for (i = 0; i < entry.pokemon.length; i++) {
+            var newEntry = entry.pokemon[i];
+            newEntries.push(new encounter(newEntry.id, 
+                            newEntry.text, 
+                            entry.location.latitude, 
+                            entry.location.longitude, 
+                            entry.datetime, 
+                            entry.trainer_level));
+        }
 
-        console.log("Updated:",entry);
-        response.send({ message: "Entered find"});
+        // Ensure there is atleast one entry, and that it has a pokemon ID and location info
+        if (newEntries[0] && newEntries[0].pokemon_id && newEntries[0].latitude && newEntries[0].longitude) {
+            // console.log(dataBase);
+            dataBase.collection('test')
+            .insert(newEntries, function(err, result) { 
+                console.log("Post insert result:", result); 
+            });
+
+            response.send({ message: "Entered find"});
+        } else {
+            console.log("Incorrect:",newEntry);
+            // response.send({ message: "Did not enter find"});
+            res.status(400).send({ error: 'Bad Request :: newEntry invalid' });
+        }
     }
 })
+
+app.get("/dump", function(request, response) { response.send({ message: "Still working on it..." }); });
 
 // Get location of encounter
 app.post('/location/:coordinates', function(request, response) {
@@ -59,7 +105,7 @@ app.post('/location/:coordinates', function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('### Node app is running on port', app.get('port'));
 });
 
 
